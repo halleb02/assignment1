7f07942610d9955a08686e09c5a3e20d98bbd0ee
Halle <hcb0046@auburn.edu>
2024-01-11 14:24:24 -0600

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Halle Paulk on 1/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
